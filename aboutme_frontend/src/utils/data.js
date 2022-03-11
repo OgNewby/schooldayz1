b566c84a19ae12345313b3ecf5a0c1a0f0442d85
Og-Newby <asiratanaka95@gmail.com>
2022-03-11 16:56:34 +0700

    Picture sharing project

---
+++ aboutme_frontend/src/utils/data.js
@@ -0,0 +1,212 @@
+export const categories = [
+    {
+      name: 'Sports',
+      image: 'https://img.freepik.com/free-vector/sport-equipment-concept_1284-13034.jpg?size=338&ext=jpg',
+    },
+    {
+      name: 'School Clubs',
+      image: 'https://ulearnmagic.com/wp-content/uploads/2021/03/how-many-clubs-are-in-a-deck-of-cards-52-standard.png?ezimgfmt=ng%3Awebp%2Fngcb14%2Frs%3Adevice%2Frscb14-1',
+    },
+    {
+      name: 'Music',
+      image: 'https://myeventsolutions.com/wp-content/uploads/2020/06/Definition-Of-The-Music.png',
+    },
+    {
+      name: 'Funny',
+      image: 'https://img.freepik.com/free-vector/funny-lol-stickers-pack_23-2148582264.jpg?w=740',
+    },
+    
+    {
+      name: 'School',
+      image: 'https://images.unsplash.com/photo-1503676260728-1c00da094a0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=822&q=80',
+    },
+    {
+      name: 'Fun at School',
+      image: 'https://img.freepik.com/free-vector/positive-background-with-sketches_23-2147629994.jpg?w=740',
+    },
+    {
+      name: 'My Weekend',
+      image: 'https://img.freepik.com/free-vector/hand-drawn-weekend-greeting_23-2147971724.jpg?w=740',
+    }, {
+      name: 'travel',
+      image: 'https://i.pinimg.com/236x/fa/95/98/fa95986f2c408098531ca7cc78aee3a4.jpg',
+    },
+    {
+      name: 'cats',
+      image: 'https://i.pinimg.com/236x/6c/3c/52/6c3c529e8dadc7cffc4fddedd4caabe1.jpg',
+    }, {
+      name: 'dogs',
+      image: 'https://i.pinimg.com/236x/1b/c8/30/1bc83077e363db1a394bf6a64b071e9f.jpg',
+    },
+    {
+      name: 'others',
+      image: 'https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg',
+    },
+  ];
+  
+  export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+        _id,
+        destination,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+        save[]{
+          _key,
+          postedBy->{
+            _id,
+            userName,
+            image
+          },
+        },
+      } `;
+  
+  export const pinDetailQuery = (pinId) => {
+    const query = `*[_type == "pin" && _id == '${pinId}']{
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      title, 
+      about,
+      category,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+     save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+      comments[]{
+        comment,
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      }
+    }`;
+    return query;
+  };
+  
+  export const pinDetailMorePinQuery = (pin) => {
+    const query = `*[_type == "pin" && category == '${pin.category}' && _id != '${pin._id}' ]{
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
+  
+  export const searchQuery = (searchTerm) => {
+    const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
+          image{
+            asset->{
+              url
+            }
+          },
+              _id,
+              destination,
+              postedBy->{
+                _id,
+                userName,
+                image
+              },
+              save[]{
+                _key,
+                postedBy->{
+                  _id,
+                  userName,
+                  image
+                },
+              },
+            }`;
+    return query;
+  };
+  
+  export const userQuery = (userId) => {
+    const query = `*[_type == "user" && _id == '${userId}']`;
+    return query;
+  };
+  
+  export const userCreatedPinsQuery = (userId) => {
+    const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
+  
+  export const userSavedPinsQuery = (userId) => {
+    const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
